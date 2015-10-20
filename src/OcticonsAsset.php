9ac8d66d92a3bf8eb9d59d4669a9e49a4d90ce00
Andrii Vasyliev <sol@hiqdev.com>
2015-10-20 15:40:42 +0000

    + asset classes

---
+++ src/OcticonsAsset.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * Yii 2 asset for bower autosize
+ *
+ * @link      https://github.com/hiqdev/yii2-asset-autosize
+ * @package   yii2-asset-autosize
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015, HiQDev (https://hiqdev.com/)
+ */
+
+namespace hiqdev\assets\octicons;
+
+class OcticonsAsset extends \yii\web\AssetBundle
+{
+    public $sourcePath = '@bower/octicons';
+
+    public $css = [
+        'octicons/octicons.css',
+    ];
+}
---
+++ src/OcticonsCDNAsset.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Yii 2 asset for bower octicons
+ *
+ * @link      https://github.com/hiqdev/yii2-asset-octicons
+ * @package   yii2-asset-octicons
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015, HiQDev (https://hiqdev.com/)
+ */
+
+namespace hiqdev\assets\octicons;
+
+class OcticonsCDNAsset extends \yii\web\AssetBundle
+{
+    public $css = [
+        '//cdnjs.cloudflare.com/ajax/libs/octicons/3.1.0/octicons.css',
+    ];
+}
